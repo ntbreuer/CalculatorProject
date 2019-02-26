826e227a8c98a5c59b0e0eeec86367262fba66d8
ntbreuer <ntbreuer@asu.edu>
2019-02-25 22:14:24 -0700

    Initial commit of Calculator

---
+++ CalulatorProject/src/cse360assign2/Calculator.java
@@ -0,0 +1,70 @@
+package cse360assign2;
+
+/**
+* This calculator program can perform basic arithmetic operations and records the history of operations
+* 
+* @author  Nic Breuer
+* @date 2/19/19
+*
+*/
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constuctor Method
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total
+	 * @param none
+	 * @return calculated total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Performs the add operation
+	 * @param integer
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Performs the subtraction operation
+	 * @param integer
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Performs the multiply operation
+	 * @param integer
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Performs the division operation
+	 * @param integer
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns a string of the history of operations
+	 * @return string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
